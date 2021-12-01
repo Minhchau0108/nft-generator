1d7ac6608ae8bc633c83ea8cd7f38b7b0962776e
MinhChau <minhchau3315@gmail.com>
2021-12-01 15:23:23 +0700

    add show images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,27 @@
-import Head from "next/head";
-import Image from "next/image";
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [images, setImages] = useState([]);
+  const fetchImages = async () => {
+    const results = await fetch("api/get-image");
+    const response = await results.json();
+    console.log(response);
+    setImages(response);
+  };
+  const handleClick = async () => {
+    await fetch("api/generate-images");
+    await fetchImages();
+  };
+
   return (
     <div className={styles.container}>
-      <button>Generator</button>
+      <div>
+        <button onClick={handleClick}>Generator</button>
+      </div>
+      {images.map((item, id) => (
+        <img src={item} alt='' key={id} />
+      ))}
     </div>
   );
 }
