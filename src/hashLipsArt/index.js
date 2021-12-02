d56d1cec988e58b0d05592cd9099fbfc2c12a510
John Trinh <john.trinh2704@gmail.com>
2021-12-02 12:31:24 +0700

    remove exec, using generate function directly

--- src/hashLipsArt/index.js
+++ src/hashLipsArt/index.js
@@ -1,6 +1,18 @@
 const { startCreating, buildSetup } = require('./src/main.js');
 
 (() => {
-  buildSetup(config = {});
-  startCreating();
+  
 })();
+const imageGenerator = (config) => {
+  return new Promise((resolve, reject) => {
+    try {
+      buildSetup(config = {});
+      startCreating();
+      resolve({message: 'Success!'})
+    } catch (error) {
+      reject(new Error('Fail!'))
+    }
+  })
+}
+
+module.exports = imageGenerator
