0fd442c7f7d0c3752a2cbad4f8089e518de6081d
John Trinh <john.trinh2704@gmail.com>
2021-12-03 13:17:40 +0700

    add cloudinary upload

---
+++ src/hashLipsArt/utils/uploadImage.js
@@ -0,0 +1,25 @@
+const cloudinary = require('cloudinary').v2
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const cloudinaryImageUploadMethod = async file => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(file, {
+      overwrite: true,
+      invalidate: true,
+      folder: 'nft'
+    }, (err, res) => {
+      if (err) {
+        reject(err)
+      }
+      resolve({
+        res: res.secure_url
+      })
+    })
+  })
+}
+
+module.exports = {cloudinaryImageUploadMethod}
